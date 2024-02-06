6038042779bc1ce6a08b6b7bb2be29feeedc7090
BenBerol <benberol40@gmail.com>
2024-02-06 16:32:04 -0500

    Clean up constants file

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,11 +17,9 @@ import edu.wpi.first.wpilibj.AnalogInput;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
 
   public final static class DrivetrainConstants{
+
     public static final double DRIFT_DEADBAND = 0.1;
     public static final double ROTATION_DEADBAND = 0.002;
 
@@ -31,31 +29,23 @@ public final class Constants {
     public static final Translation2d BACK_RIGHT_LOC = new Translation2d(-0.238125, -0.238125);
     public static final double BACK_RIGHT_ENCODER_OFFSET = 0.669;
 
-    
-
-
     public static final int FRONT_RIGHT_ANGLE_ID = 3;
     public static final int FRONT_RIGHT_SPEED_ID = 4;
     public static final AnalogInput FRONT_RIGHT_ENCODER = new AnalogInput(0);
     public static final Translation2d FRONT_RIGHT_LOC = new Translation2d(0.238125, -0.238125);
     public static final double FRONT_RIGHT_ENCODER_OFFSET = 0.616;
-
     
-    
-
     public static final int BACK_LEFT_ANGLE_ID = 7;
     public static final int BACK_LEFT_SPEED_ID = 8;
     public static final AnalogInput BACK_LEFT_ENCODER = new AnalogInput(2);
     public static final Translation2d BACK_LEFT_LOC = new Translation2d(-0.238125, 0.238125);
     public static final double BACK_LEFT_ENCODER_OFFSET = 0.903;
-
     
     public static final int FRONT_LEFT_ANGLE_ID = 1;
     public static final int FRONT_LEFT_SPEED_ID = 2;
     public static final AnalogInput FRONT_LEFT_ENCODER = new AnalogInput(1);
     public static final Translation2d FRONT_LEFT_LOC = new Translation2d(0.238125, 0.238125);
     public static final double FRONT_LEFT_ENCODER_OFFSET = 0.862;
-
     
     public final static SwerveDriveKinematics SWERVE_KINEMATICS = new SwerveDriveKinematics(
       FRONT_LEFT_LOC,
