4e101654070190751c401f4d137f6786b6523acb
BenBerol <benberol40@gmail.com>
2024-02-09 17:17:01 -0500

    Remove command requirements

--- src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -10,6 +10,7 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriverConstants;
 import frc.robot.Constants.DrivetrainConstants;
@@ -132,14 +133,14 @@ public class SwerveDriveSubsystem extends SubsystemBase {
     }
 
     public Command toggleFieldCentric() {
-        return runOnce(() -> {
+        return Commands.runOnce(() -> {
             fieldCentric = !fieldCentric;
             ntIsFieldCentric.setBoolean(fieldCentric);
         });
     }
 
     public Command resetFieldCentric() {
-        return runOnce(() -> {
+        return Commands.runOnce(() -> {
             odometry.zeroHeading();
             resetLockRot();
         });
